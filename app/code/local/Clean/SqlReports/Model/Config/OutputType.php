44b9410969a9b2cea628bb34e7a20c2b386b4233
Allan MacGregor <dev@allanmacgregor.com>
2014-05-11 12:31:13 -0700

    Small helper refactor to the Config OutputType

--- app/code/local/Clean/SqlReports/Model/Config/OutputType.php
+++ app/code/local/Clean/SqlReports/Model/Config/OutputType.php
@@ -32,28 +32,30 @@ class Clean_SqlReports_Model_Config_OutputType extends Mage_Core_Model_Config_Da
 
     public function toOptionArray()
     {
+        $helper = Mage::helper('cleansql');
+        
         return array(
-            array("title"  =>Mage::helper('cleansql')->__('Simple Charts'), 'label' => Mage::helper('cleansql')->__('Simple Charts'), 'value' => array(
-                array('value' =>self::TYPE_PIE_CHART, 'title' =>Mage::helper('cleansql')->__('Pie Chart'), 'label' => Mage::helper('cleansql')->__('Pie Chart')),
-                array('value' =>self::TYPE_BAR_CHART, 'title' =>Mage::helper('cleansql')->__('Bar Chart'), 'label' => Mage::helper('cleansql')->__('Bar Chart')),
-                array('value' =>self::TYPE_GEO_CHART, 'title' =>Mage::helper('cleansql')->__('Geo Chart'), 'label' => Mage::helper('cleansql')->__('Geo Chart')),
-                array('value' =>self::TYPE_AREA_CHART, 'title' =>Mage::helper('cleansql')->__('Area Chart'), 'label' => Mage::helper('cleansql')->__('Area Chart')),
-                array('value' =>self::TYPE_LINE_CHART, 'title' =>Mage::helper('cleansql')->__('Line Chart'), 'label' => Mage::helper('cleansql')->__('Line Chart')),
-                array('value' =>self::TYPE_COMBO_CHART, 'title' =>Mage::helper('cleansql')->__('Combo Chart'), 'label' => Mage::helper('cleansql')->__('Combo Chart')),
-                array('value' =>self::TYPE_BUBBLE_CHART, 'title' =>Mage::helper('cleansql')->__('Bubble Chart'), 'label' => Mage::helper('cleansql')->__('Bubble Chart')),
-                array('value' =>self::TYPE_COLUMN_CHART, 'title' =>Mage::helper('cleansql')->__('Column Chart'), 'label' => Mage::helper('cleansql')->__('Column Chart')),
-                array('value' =>self::TYPE_TREEMAP_CHART, 'title' =>Mage::helper('cleansql')->__('TreeMap Chart'), 'label' => Mage::helper('cleansql')->__('TreeMap Chart')),
-                array('value' =>self::TYPE_SCATTER_CHART, 'title' =>Mage::helper('cleansql')->__('Scatter Chart'), 'label' => Mage::helper('cleansql')->__('Scatter Chart')),
-                array('value' =>self::TYPE_CANDLESTICK_CHART, 'title' =>Mage::helper('cleansql')->__('Candlestick Chart'), 'label' => Mage::helper('cleansql')->__('Candlestick Chart')),
-                array('value' =>self::TYPE_STEPPEDAREA_CHART, 'title' =>Mage::helper('cleansql')->__('SteppedArea Chart'), 'label' => Mage::helper('cleansql')->__('SteppedArea Chart')),
+            array("title"  => $helper->__('Simple Charts'), 'label' => $helper->__('Simple Charts'), 'value' => array(
+                array('value' =>self::TYPE_PIE_CHART, 'title' => $helper->__('Pie Chart'), 'label' => $helper->__('Pie Chart')),
+                array('value' =>self::TYPE_BAR_CHART, 'title' => $helper->__('Bar Chart'), 'label' => $helper->__('Bar Chart')),
+                array('value' =>self::TYPE_GEO_CHART, 'title' => $helper->__('Geo Chart'), 'label' => $helper->__('Geo Chart')),
+                array('value' =>self::TYPE_AREA_CHART, 'title' => $helper->__('Area Chart'), 'label' => $helper->__('Area Chart')),
+                array('value' =>self::TYPE_LINE_CHART, 'title' => $helper->__('Line Chart'), 'label' => $helper->__('Line Chart')),
+                array('value' =>self::TYPE_COMBO_CHART, 'title' => $helper->__('Combo Chart'), 'label' => $helper->__('Combo Chart')),
+                array('value' =>self::TYPE_BUBBLE_CHART, 'title' => $helper->__('Bubble Chart'), 'label' => $helper->__('Bubble Chart')),
+                array('value' =>self::TYPE_COLUMN_CHART, 'title' => $helper->__('Column Chart'), 'label' => $helper->__('Column Chart')),
+                array('value' =>self::TYPE_TREEMAP_CHART, 'title' => $helper->__('TreeMap Chart'), 'label' => $helper->__('TreeMap Chart')),
+                array('value' =>self::TYPE_SCATTER_CHART, 'title' => $helper->__('Scatter Chart'), 'label' => $helper->__('Scatter Chart')),
+                array('value' =>self::TYPE_CANDLESTICK_CHART, 'title' => $helper->__('Candlestick Chart'), 'label' => $helper->__('Candlestick Chart')),
+                array('value' =>self::TYPE_STEPPEDAREA_CHART, 'title' => $helper->__('SteppedArea Chart'), 'label' => $helper->__('SteppedArea Chart')),
             )),
 
-            array("title"  =>Mage::helper('cleansql')->__('Complex Charts'), 'label' => Mage::helper('cleansql')->__('Complex Charts'), 'value' => array(
-                array('value' =>self::TYPE_ORG_CHART, 'title' =>Mage::helper('cleansql')->__('Org Chart'), 'label' => Mage::helper('cleansql')->__('Org Chart')),
-                array('value' =>self::TYPE_SANKEY_CHART, 'title' =>Mage::helper('cleansql')->__('Sankey Chart'), 'label' => Mage::helper('cleansql')->__('Sankey Chart')),
-                array('value' =>self::TYPE_TIMELINE_CHART, 'title' =>Mage::helper('cleansql')->__('Timeline Chart'), 'label' => Mage::helper('cleansql')->__('Timeline Chart')),
-                array('value' =>self::TYPE_HISTOGRAM_CHART, 'title' =>Mage::helper('cleansql')->__('Histogram Chart'), 'label' => Mage::helper('cleansql')->__('Histrogram Chart')),
-                array('value' =>self::TYPE_CALENDAR_CHART, 'title' =>Mage::helper('cleansql')->__('Calendar Chart'), 'label' => Mage::helper('cleansql')->__('Calendar Chart'))
+            array("title"  => $helper->__('Complex Charts'), 'label' => $helper->__('Complex Charts'), 'value' => array(
+                array('value' =>self::TYPE_ORG_CHART, 'title' => $helper->__('Org Chart'), 'label' => $helper->__('Org Chart')),
+                array('value' =>self::TYPE_SANKEY_CHART, 'title' => $helper->__('Sankey Chart'), 'label' => $helper->__('Sankey Chart')),
+                array('value' =>self::TYPE_TIMELINE_CHART, 'title' => $helper->__('Timeline Chart'), 'label' => $helper->__('Timeline Chart')),
+                array('value' =>self::TYPE_HISTOGRAM_CHART, 'title' => $helper->__('Histogram Chart'), 'label' => $helper->__('Histrogram Chart')),
+                array('value' =>self::TYPE_CALENDAR_CHART, 'title' => $helper->__('Calendar Chart'), 'label' => $helper->__('Calendar Chart'))
             ))
         );
     }
