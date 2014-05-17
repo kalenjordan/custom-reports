174106d279c2070b1aa74a167ef7b8b5ace1b691
Lee Saferite <lee.saferite@aoe.com>
2014-05-17 13:17:08 -0400

    Cleaning up after rebase to latest upstream code

--- app/code/local/Clean/SqlReports/Helper/Data.php
+++ app/code/local/Clean/SqlReports/Helper/Data.php
@@ -116,40 +116,4 @@ class Clean_SqlReports_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return Mage::getSingleton('admin/session');
     }
-
-    /**
-     * @param $report Clean_SqlReports_Model_Report
-     * @return bool
-     */
-    public function getPrimaryReportRoute($report)
-    {
-        if ($report->hasChart()) {
-            return 'viewChart';
-        } else {
-            return 'viewTable';
-        }
-    }
-    /**
-     * get active db connection resource config nodes
-     * @return Mage_Core_Model_Config_Element
-     */
-    public function getConnectionResourceConfig()
-    {
-        $resourceConfig = Mage::getConfig()->getXpath('global/resources/*[child::connection and descendant::active=1]');
-        return $resourceConfig;
-    }
-    /**
-     * get default connection resource model
-     * @return Magento_Db_Adapter_Pdo_Mysql
-     */
-    public function getDefaultConnection()
-    {
-        /* @var $resource Mage_Core_Model_Resource */
-        $resource       = Mage::getSingleton('core/resource');
-        $connectionName = Mage::getStoreConfig('reports/cleansql/default_connection');
-        if (!$connectionName) {
-            $connectionName = 'core_read';
-        }
-        return $resource->getConnection($connectionName);
-    }
 }
